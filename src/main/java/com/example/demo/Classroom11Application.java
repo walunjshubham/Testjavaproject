f826064114b2e4ec9ed2759da06aaef57e7ca1ea
shubham <shubhamwalunj25@gmail.com>
2022-01-23 22:09:08 +0530

    project commited

---
+++ src/main/java/com/example/demo/Classroom11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classroom11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classroom11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Classroom11ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Classroom11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("inside the src / test /java class....");
+	}
+	
+
+}
